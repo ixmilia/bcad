0c1b0213caafd5c77202a54f7b9fbdfcea413b2d
Brett V. Forsgren <brett.forsgren@outlook.com>
2012-04-01 10:40:49 -0700

    moved point extensions

---
+++ BCad.Core/Extensions/PointExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Media.Media3D;
+
+namespace BCad
+{
+    public static class PointExtensions
+    {
+        public static System.Drawing.Point ToPoint(this Point3D vector)
+        {
+            return new System.Drawing.Point((int)vector.X, (int)vector.Y);
+        }
+    }
+}
