1f261c0cc4872d172a4bb58b1fef417410d61a64
Brett V. Forsgren <brett.forsgren@outlook.com>
2012-04-06 20:58:40 -0700

    added custom export attribute for controls

---
+++ BCad.Core/UI/ExportControlFactoryAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.Composition;
+
+namespace BCad.UI
+{
+    [MetadataAttribute]
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class ExportControlFactoryAttribute : ExportAttribute
+    {
+        public string ControlId { get; private set; }
+
+        public string Title { get; private set; }
+
+        public ExportControlFactoryAttribute(string controlId, string title)
+            : base(typeof(IControlFactory))
+        {
+            this.ControlId = controlId;
+            this.Title = title;
+        }
+    }
+}
--- BCad.UI/LayerManagerControlFactory.cs
+++ BCad.UI/LayerManagerControlFactory.cs
@@ -2,9 +2,7 @@
 
 namespace BCad.UI
 {
-    [Export(typeof(IControlFactory))]
-    [ExportMetadata("ControlId", "Default")]
-    [ExportMetadata("Title", "Layers")]
+    [ExportControlFactory("Default", "Layers")]
     internal class LayerManagerControlFactory : IControlFactory
     {
         [Import]
