9e9c2d1ed0f5d4275b4b2d1524efef2350f71062
Brett V. Forsgren <brett.forsgren@outlook.com>
2013-01-31 17:28:15 -0800

    prompt for viewport in plot

--- BCad.Core/Commands/PlotCommand.cs
+++ BCad.Core/Commands/PlotCommand.cs
@@ -5,6 +5,7 @@ using System.IO;
 using System.Linq;
 using BCad.FileHandlers;
 using BCad.Helpers;
+using BCad.Primitives;
 using BCad.Services;
 
 namespace BCad.Commands
@@ -14,6 +15,9 @@ namespace BCad.Commands
     {
         [Import]
         private IWorkspace Workspace = null;
+
+        [Import]
+        private IInputService InputService = null;
 
         [Import]
         private IExportService ExportService = null;
@@ -28,7 +32,7 @@ namespace BCad.Commands
                 filename = (string)arg;
             if (filename == null)
             {
-                filename = UIHelper.GetFilenameFromUserForOpen(FilePlotters.Select(f => new FileSpecification(f.Metadata.DisplayName, f.Metadata.FileExtensions)));
+                filename = UIHelper.GetFilenameFromUserForSave(FilePlotters.Select(f => new FileSpecification(f.Metadata.DisplayName, f.Metadata.FileExtensions)));
                 if (filename == null)
                     return false;
             }
@@ -38,12 +42,38 @@ namespace BCad.Commands
             if (plotter == null) // invalid file selected
                 throw new Exception("Unknown file extension " + extension);
 
-            // TODO: prompt for viewport, width, and height
-            var viewPort = Workspace.ActiveViewPort;
-            var width = 1.0;
-            var height = 1.0;
+            // TODO: generalize getting viewports for zoom, etc.
+            // prompt for viewport
+            var firstPoint = InputService.GetPoint(new UserDirective("First corner of view box"));
+            if (firstPoint.Cancel || !firstPoint.HasValue)
+                return false;
 
-            using (var file = new FileStream(filename, FileMode.Open))
+            var secondPoint = InputService.GetPoint(new UserDirective("Second corner of view box"), (p) =>
+                {
+                    var a = firstPoint.Value;
+                    var b = new Point(p.X, firstPoint.Value.Y, firstPoint.Value.Z);
+                    var c = new Point(p.X, p.Y, firstPoint.Value.Z);
+                    var d = new Point(firstPoint.Value.X, p.Y, firstPoint.Value.Z);
+                    return new[]
+                    {
+                        new PrimitiveLine(a, b),
+                        new PrimitiveLine(b, c),
+                        new PrimitiveLine(c, d),
+                        new PrimitiveLine(d, a)
+                    };
+                });
+            if (secondPoint.Cancel || !secondPoint.HasValue)
+                return false;
+
+            // prompt for viewport, width, and height
+            var size = secondPoint.Value - firstPoint.Value;
+            var width = Math.Abs(size.X);
+            var height = Math.Abs(size.Y);
+            var viewPort = new ViewPort(
+                new Point(Math.Min(firstPoint.Value.X, secondPoint.Value.X), Math.Min(firstPoint.Value.Y, secondPoint.Value.Y), firstPoint.Value.Z),
+                Workspace.ActiveViewPort.Sight, Workspace.ActiveViewPort.Up, height);
+
+            using (var file = new FileStream(filename, FileMode.Create))
             {
                 var entities = ExportService.ProjectTo2D(Workspace.Drawing, viewPort);
                 plotter.Plot(entities, width, height, file);
