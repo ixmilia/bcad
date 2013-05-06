b7f72a206b713458be8ad8a7d33f23261470e25c
Brett V. Forsgren <brett.forsgren@outlook.com>
2013-05-06 15:14:34 -0700

    combining common trim/extend logic

--- BCad.Core/Commands/TrimCommand.cs
+++ BCad.Core/Commands/TrimCommand.cs
@@ -1,62 +1,24 @@
 ﻿using System.Collections.Generic;
-using System.ComponentModel.Composition;
-using System.Linq;
-using System.Threading.Tasks;
 using BCad.Entities;
-using BCad.Services;
 
 namespace BCad.Commands
 {
     [ExportCommand("Edit.Trim", "TRIM", "trim", "tr")]
-    public class TrimCommand : ICommand
+    public class TrimCommand : AbstractTrimExtendCommand
     {
-        [Import]
-        private IInputService InputService = null;
-
-        [Import]
-        private IEditService EditService = null;
-
-        [Import]
-        private IWorkspace Workspace = null;
-
-        public async Task<bool> Execute(object arg)
+        protected override string GetBoundsText()
         {
-            var boundaries = await InputService.GetEntities("Select cutting edges");
-            if (boundaries.Cancel || !boundaries.HasValue || !boundaries.Value.Any())
-            {
-                return false;
-            }
+            return "Select cutting edges";
+        }
 
-            var boundaryPrimitives = boundaries.Value.SelectMany(b => b.GetPrimitives());
-            Workspace.SelectedEntities.Set(boundaries.Value);
+        protected override string GetTrimExtendText()
+        {
+            return "Entity to trim";
+        }
 
-            var drawing = Workspace.Drawing;
-            var directive = new UserDirective("Entity to trim");
-            var selected = await InputService.GetEntity(directive);
-            IEnumerable<Entity> removed;
-            IEnumerable<Entity> added;
-            string entityLayerName;
-            while (!selected.Cancel && selected.HasValue)
-            {
-                entityLayerName = drawing.ContainingLayer(selected.Value.Entity).Name;
-                EditService.Trim(selected.Value, boundaryPrimitives, out removed, out added);
-
-                foreach (var ent in removed)
-                    drawing = drawing.Remove(ent);
-
-                foreach (var ent in added)
-                    drawing = drawing.Add(drawing.Layers.GetValue(entityLayerName), ent);
-
-                // commit the change
-                if (Workspace.Drawing != drawing)
-                    Workspace.Update(drawing: drawing);
-
-                // get next entity to trim
-                selected = await InputService.GetEntity(directive);
-            }
-
-            Workspace.SelectedEntities.Clear();
-            return true;
+        protected override void DoTrimExtend(SelectedEntity selectedEntity, IEnumerable<Primitives.IPrimitive> boundaryPrimitives, out IEnumerable<Entity> removed, out IEnumerable<Entity> added)
+        {
+            EditService.Trim(selectedEntity, boundaryPrimitives, out removed, out added);
         }
     }
 }
