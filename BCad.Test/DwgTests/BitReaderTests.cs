3940827b5c090677b3398549c85a394fef335d7f
Brett V. Forsgren <brett.forsgren@outlook.com>
2013-06-14 10:21:35 -0700

    initial dwg support

---
+++ BCad.Test/DwgTests/BitReaderTests.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCad.Dwg;
+using Xunit;
+
+namespace BCad.Test.DwgTests
+{
+    public class BitReaderTests
+    {
+        [Fact]
+        public void ReadBitTest()
+        {
+            var reader = new BitReader(new MemoryStream(new byte[] { 0x5F, 0xF5 }));
+            Assert.Equal(0, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(0, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+
+            Assert.Equal(0, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(0, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+        }
+
+        [Fact]
+        public void ReadWholeBytesTest()
+        {
+            var reader = new BitReader(new MemoryStream(new byte[] { 0x5F, 0xF5 }));
+            Assert.Equal((byte)0x5F, reader.ReadByte());
+            Assert.Equal((byte)0xF5, reader.ReadByte());
+        }
+
+        [Fact]
+        public void ReadBytesWithOffsetTest()
+        {
+            var reader = new BitReader(new MemoryStream(new byte[] { 0x5F, 0xF5 }));
+            Assert.Equal(0, reader.ReadBit());
+            Assert.Equal((byte)0xBF, reader.ReadByte());
+        }
+
+        [Fact]
+        public void ReadBytesWithHalfOffsetTest()
+        {
+            var reader = new BitReader(new MemoryStream(new byte[] { 0x5F, 0xF5 }));
+            Assert.Equal(0, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(0, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+
+            Assert.Equal((byte)0xFF, reader.ReadByte());
+
+            Assert.Equal(0, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+            Assert.Equal(0, reader.ReadBit());
+            Assert.Equal(1, reader.ReadBit());
+        }
+    }
+}
---
+++ BCad.Test/DwgTests/LoadTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BCad.Dwg;
+using Xunit;
+
+namespace BCad.Test.DwgTests
+{
+    public class LoadTests
+    {
+        [Fact]
+        public void LoadTest()
+        {
+            //var fileName = @"C:\Users\brettfo\Source\Repos\libredwg\examples\example.dwg";
+            var fileName = @"C:\Users\brettfo\Desktop\brettfo2.dwg";
+            var file = DwgFile.Load(new FileStream(fileName, FileMode.Open));
+        }
+    }
+}
