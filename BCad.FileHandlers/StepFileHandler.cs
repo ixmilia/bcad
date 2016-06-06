88dd321d7851c237de71fb503827f119e9dafafa
Brett V. Forsgren <brett.forsgren@outlook.com>
2016-06-06 15:48:57 -0700

    support STEP ORIENTED_EDGE items

--- BCad.FileHandlers/StepFileHandler.cs
+++ BCad.FileHandlers/StepFileHandler.cs
@@ -36,33 +36,19 @@ namespace BCad.FileHandlers
                         }
                     case StepItemType.EdgeCurve:
                         {
-                            var edgeCurve = (StepEdgeCurve)item;
-                            if (edgeCurve.EdgeGeometry is StepCircle)
-                            {
-                                var circle = (StepCircle)edgeCurve.EdgeGeometry;
-                                var center = circle.Position.Location;
-                                var normal = ToVector(circle.Position.RefDirection);
-                                normal = Vector.ZAxis;
-                                var primitiveCircle = new PrimitiveEllipse(ToPoint(center), circle.Radius, normal);
-                                var toUnit = primitiveCircle.FromUnitCircle.Inverse();
-                                var startPoint = toUnit.Transform(ToPoint((StepVertexPoint)edgeCurve.EdgeStart));
-                                var endPoint = toUnit.Transform(ToPoint((StepVertexPoint)edgeCurve.EdgeEnd));
-                                var startAngle = Math.Atan2(startPoint.Y, startPoint.X).CorrectAngleRadians() * MathHelper.RadiansToDegrees;
-                                var endAngle = Math.Atan2(endPoint.Y, endPoint.X).CorrectAngleRadians() * MathHelper.RadiansToDegrees;
-                                var arc = new Arc(primitiveCircle.Center, circle.Radius, startAngle, endAngle, primitiveCircle.Normal, null);
-                                layer = layer.Add(arc);
-                            }
-                            else if (edgeCurve.EdgeGeometry is StepLine)
-                            {
-                                var stepLine = (StepLine)edgeCurve.EdgeGeometry;
-                                layer = layer.Add(ToLine(stepLine));
-                            }
+                            layer = layer.Add(ToEntity((StepEdge)item));
                             break;
                         }
                     case StepItemType.Line:
                         {
                             var line = ToLine((StepLine)item);
                             layer = layer.Add(line);
+                            break;
+                        }
+                    case StepItemType.OrientedEdge:
+                        {
+                            var orientedEdge = (StepOrientedEdge)item;
+                            layer = layer.Add(ToEntity(orientedEdge.EdgeElement));
                             break;
                         }
                 }
@@ -87,6 +73,54 @@ namespace BCad.FileHandlers
             var p1 = ToPoint(stepLine.Point);
             var p2 = p1 + new Vector(dx, dy, dz);
             return new Line(p1, p2, null);
+        }
+
+        private static Entity ToEntity(StepEdge edge)
+        {
+            switch (edge.ItemType)
+            {
+                case StepItemType.EdgeCurve:
+                    {
+                        var edgeCurve = (StepEdgeCurve)edge;
+                        if (edgeCurve.EdgeGeometry is StepCircle)
+                        {
+                            var circle = (StepCircle)edgeCurve.EdgeGeometry;
+                            var center = circle.Position.Location;
+                            var normal = ToVector(circle.Position.RefDirection);
+                            normal = Vector.ZAxis;
+                            var primitiveCircle = new PrimitiveEllipse(ToPoint(center), circle.Radius, normal);
+                            var toUnit = primitiveCircle.FromUnitCircle.Inverse();
+                            var startPoint = toUnit.Transform(ToPoint((StepVertexPoint)edgeCurve.EdgeStart));
+                            var endPoint = toUnit.Transform(ToPoint((StepVertexPoint)edgeCurve.EdgeEnd));
+                            var startAngle = Math.Atan2(startPoint.Y, startPoint.X).CorrectAngleRadians() * MathHelper.RadiansToDegrees;
+                            var endAngle = Math.Atan2(endPoint.Y, endPoint.X).CorrectAngleRadians() * MathHelper.RadiansToDegrees;
+                            return new Arc(primitiveCircle.Center, circle.Radius, startAngle, endAngle, primitiveCircle.Normal, null);
+                        }
+                        else if (edgeCurve.EdgeGeometry is StepLine)
+                        {
+                            var stepLine = (StepLine)edgeCurve.EdgeGeometry;
+                            Line line;
+                            if (edgeCurve.EdgeStart == null || edgeCurve.EdgeEnd == null)
+                            {
+                                // use auto values
+                                line = ToLine(stepLine);
+                            }
+                            else
+                            {
+                                // use explicit values
+                                var p1 = ToPoint((StepVertexPoint)edgeCurve.EdgeStart);
+                                var p2 = ToPoint((StepVertexPoint)edgeCurve.EdgeEnd);
+                                line = new Line(p1, p2, null);
+                            }
+
+                            return line;
+                        }
+
+                        break;
+                    }
+            }
+
+            return null;
         }
 
         private static Point ToPoint(StepCartesianPoint point)
