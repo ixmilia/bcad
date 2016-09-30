ebc6f232bf9b8b058a939b6f84e5e784cbbc3862
Brett V. Forsgren <brett.forsgren@outlook.com>
2016-09-30 14:13:25 -0700

    update restore script

--- Restore.cmd
+++ Restore.cmd
@@ -2,3 +2,9 @@ set nugetexe=%~dp0.nuget\NuGet.exe
 
 %nugetexe% restore %~dp0BCad.sln
 %Nugetexe% restore %~dp0BCad\packages.config -SolutionDir %~dp0
+
+dotnet restore BCad\project.json
+dotnet restore BCad.Core\project.json
+dotnet restore BCad.FileHandlers\project.json
+dotnet restore IxMilia.Iges\src\IxMilia.Iges\project.json
+dotnet restore IxMilia.Step\src\IxMilia.Step\project.json
