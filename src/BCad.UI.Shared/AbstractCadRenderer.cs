ef88fa0c1385d6526856b5040a93f42a14f30341
Brett V. Forsgren <brett.forsgren@outlook.com>
2016-10-26 16:45:17 -0700

    add copyright to BCad.UI.Shared project

--- src/BCad.UI.Shared/AbstractCadRenderer.cs
+++ src/BCad.UI.Shared/AbstractCadRenderer.cs
@@ -1,4 +1,6 @@
-﻿#if WINDOWS_UWP
+﻿// Copyright (c) IxMilia.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+#if WINDOWS_UWP
 using Windows.UI.Xaml.Controls;
 #elif WPF
 using System.Windows.Controls;
--- src/BCad.UI.Shared/ViewPane.TransformedSnapPoint.cs
+++ src/BCad.UI.Shared/ViewPane.TransformedSnapPoint.cs
@@ -1,4 +1,6 @@
-﻿using BCad.SnapPoints;
+﻿// Copyright (c) IxMilia.  All Rights Reserved.  Licensed under the Apache License, Version 2.0.  See License.txt in the project root for license information.
+
+using BCad.SnapPoints;
 
 namespace BCad.UI.Shared
 {
