9a38e7f1a93f2f63d2ee79ffee3808be89b5731b
Brett V. Forsgren <brett.forsgren@outlook.com>
2017-08-08 10:45:25 -0700

    add `IxMilia.` prefix to all assemblies except BCad.exe

--- build-and-test.sh
+++ build-and-test.sh
@@ -1,7 +1,7 @@
 #!/bin/sh -e
 
-CORE_TEST=./src/BCad.Core.Test/BCad.Core.Test.csproj
-FILE_HANDLER_TEST=./src/BCad.FileHandlers.Test/BCad.FileHandlers.Test.csproj
+CORE_TEST=./src/IxMilia.BCad.Core.Test/IxMilia.BCad.Core.Test.csproj
+FILE_HANDLER_TEST=./src/IxMilia.BCad.FileHandlers.Test/IxMilia.BCad.FileHandlers.Test.csproj
 dotnet restore $CORE_TEST
 dotnet restore $FILE_HANDLER_TEST
 dotnet test $CORE_TEST
