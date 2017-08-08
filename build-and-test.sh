2a0dccb1fbdf2c7449515c239643c798f7bea5c1
Brett V. Forsgren <brett.forsgren@outlook.com>
2017-08-08 13:11:31 -0700

    simplify build scripts

--- build-and-test.sh
+++ build-and-test.sh
@@ -2,7 +2,10 @@
 
 CORE_TEST=./src/IxMilia.BCad.Core.Test/IxMilia.BCad.Core.Test.csproj
 FILE_HANDLER_TEST=./src/IxMilia.BCad.FileHandlers.Test/IxMilia.BCad.FileHandlers.Test.csproj
-dotnet restore $CORE_TEST
+
+# only need to restore/build this project since everything else cascades off of it
 dotnet restore $FILE_HANDLER_TEST
+dotnet build $FILE_HANDLER_TEST
+
 dotnet test $CORE_TEST
 dotnet test $FILE_HANDLER_TEST
