fb6fe95f404bc93aac83c6abfab6a10ec5bde63a
Brett V. Forsgren <brett.forsgren@outlook.com>
2017-12-14 14:28:38 -0800

    fix desktop deployment

--- src/deploy.bat
+++ src/deploy.bat
@@ -11,6 +11,8 @@ exit /b 1
 :gooddir
 set configuration=%2
 if [%configuration%]==[] set configuration=Debug
+set targetframework=%3
+if [%targetframework%]==[] set targetframework=net45
 
 if not exist "%deploydir%" mkdir "%deploydir%"
-for /F "tokens=*" %%f in (%~dp0\deployment-files-%deploytype%.txt) do copy "%~dp0BCad\bin\%configuration%\%%f" "%deploydir%"
+for /F "tokens=*" %%f in (%~dp0\deployment-files-%deploytype%.txt) do copy "%~dp0BCad\bin\%configuration%\%targetframework%\%%f" "%deploydir%"
