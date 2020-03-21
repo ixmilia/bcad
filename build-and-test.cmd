9ffdced13dc55b00a64b96ec8dc900feb20a0af5
Brett V. Forsgren <brett.forsgren@outlook.com>
2020-03-21 12:21:31 -0700

    re-work github actions ci

--- build-and-test.cmd
+++ build-and-test.cmd
@@ -1,7 +1,33 @@
 @echo off
 
+set configuration=Debug
+set runtests=true
+
+:parseargs
+if "%1" == "" goto argsdone
+if /i "%1" == "-c" goto set_configuration
+if /i "%1" == "--configuration" goto set_configuration
+if /i "%1" == "-notest" goto set_notest
+if /i "%1" == "--notest" goto set_notest
+
+echo Unsupported argument: %1
+goto error
+
+:set_configuration
+set configuration=%2
+shift
+shift
+goto parseargs
+
+:set_notest
+set runtests=false
+shift
+goto parseargs
+
+:argsdone
+
 :: IxMilia.Dxf needs a custom invocation
-call %~dp0src\IxMilia.Dxf\build-and-test.cmd -notest
+call %~dp0src\IxMilia.Dxf\build-and-test.cmd -c %configuration% -notest
 if errorlevel 1 echo Error pre-building IxMilia.Dxf && goto error
 
 :: restore packages
@@ -9,12 +35,14 @@ dotnet restore
 if errorlevel 1 echo Error restoring packages && goto error
 
 :: build .NET
-dotnet build
+dotnet build -c %configuration%
 if errorlevel 1 echo Error building solution && goto error
 
 :: test
-dotnet test --no-restore --no-build
-if errorlevel 1 echo Error running tests && goto error
+if /i "%runtests%" == "true" (
+    dotnet test --no-restore --no-build -c %configuration%
+    if errorlevel 1 echo Error running tests && goto error
+)
 
 :: build electron
 pushd %~dp0src\bcad
--- build-and-test.sh
+++ build-and-test.sh
@@ -2,17 +2,39 @@
 
 _SCRIPT_DIR="$( cd -P -- "$(dirname -- "$(command -v -- "$0")")" && pwd -P )"
 
+CONFIGURATION=Debug
+RUNTESTS=true
+
+while [ $# -gt 0 ]; do
+  case "$1" in
+    --configuration|-c)
+      CONFIGURATION=$2
+      shift
+      ;;
+    --notest)
+      RUNTESTS=false
+      ;;
+    *)
+      echo "Invalid argument: $1"
+      exit 1
+      ;;
+  esac
+  shift
+done
+
 # IxMilia.Dxf needs a custom invocation
-"$_SCRIPT_DIR/src/IxMilia.Dxf/build-and-test.sh" --notest
+"$_SCRIPT_DIR/src/IxMilia.Dxf/build-and-test.sh" --notest --configuration $CONFIGURATION
 
 # restore packages
 dotnet restore
 
 # build .NET
-dotnet build
+dotnet build -c $CONFIGURATION
 
 # test
-dotnet test --no-restore --no-build
+if [ "$RUNTESTS" = "true" ]; then
+  dotnet test --no-restore --no-build -c $CONFIGURATION
+fi
 
 # build electron
 pushd "$_SCRIPT_DIR/src/bcad"
