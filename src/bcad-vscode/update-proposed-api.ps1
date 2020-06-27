3aedc1c52d3858db52d1696649231a9c8348eccb
Brett V. Forsgren <brett.forsgren@outlook.com>
2020-06-26 21:03:33 -0700

    consume stable vscode types

--- src/bcad-vscode/update-proposed-api.ps1
+++
@@ -1,2 +0,0 @@
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/master/src/vs/vscode.d.ts" -OutFile "$PSScriptRoot\src\vscode.d.ts"
-Invoke-WebRequest -Uri "https://raw.githubusercontent.com/microsoft/vscode/master/src/vs/vscode.proposed.d.ts" -OutFile "$PSScriptRoot\src\vscode.proposed.d.ts"
