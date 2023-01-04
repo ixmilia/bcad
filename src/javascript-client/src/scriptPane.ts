1d8693ed530f069623bed1e32e53a85e86e86722
Brett V. Forsgren <brett.forsgren@outlook.com>
2023-01-04 11:37:14 -0700

    use monaco editor

--- src/javascript-client/src/scriptPane.ts
+++ src/javascript-client/src/scriptPane.ts
@@ -1,23 +1,67 @@
 import { Client } from './client';
+import * as monaco from 'monaco-editor';
 
 export class ScriptPane {
     constructor(client: Client) {
         const scriptPane = <HTMLDivElement>document.getElementById("script-pane");
+        const scriptEditor = <HTMLDivElement>document.getElementById("script-content");
+        ScriptPane.captureUserGestures(scriptPane);
+
+        // prepare monaco editor
+        const languageId = 'cad';
+        monaco.languages.register({
+            id: languageId
+        });
+        monaco.languages.setMonarchTokensProvider(languageId, {
+            ignoreCase: true,
+            keywords: [
+                // TODO: expand this
+                'LINE',
+                'CIRCLE',
+            ],
+            tokenizer: {
+                root: [
+                    [/[+-]?\d+(?:(?:\.\d*)?(?:[eE][+-]?\d+)?)?/, 'number'],
+                    [/"[^"]*"/, 'string'],
+                    [/"[^"]*$/, 'string.invalid'],
+                    [/^;.*/, 'comment'],
+                    [/[a-zA-Z_#][a-zA-Z0-9_\-\?\!\*]*/, {
+                        cases: {
+                            '@keywords': 'keyword',
+                            '@default': 'identifier'
+                        }
+                    }],
+                ],
+            }
+        });
+        const editor = monaco.editor.create(scriptEditor, {
+            value: '; add your script here',
+            language: languageId,
+        });
+
         const showScriptToggle = <HTMLInputElement>document.getElementById('showScript');
         showScriptToggle.addEventListener('change', ev => {
             if (showScriptToggle.checked) {
                 scriptPane.style.display = 'block';
+                editor.layout();
             } else {
                 scriptPane.style.display = 'none';
             }
         });
 
-        const textArea = <HTMLTextAreaElement>document.getElementById("script-content");
-        ScriptPane.captureUserGestures(scriptPane);
+        // prepare script execution
+        function executeScript() {
+            const scriptContent = editor.getValue();
+            client.executeScript(scriptContent);
+        }
         const runScriptButton = <HTMLButtonElement>document.getElementById("run-script");
         runScriptButton.addEventListener('click', ev => {
-            const scriptContent = textArea.value;
-            client.executeScript(scriptContent);
+            executeScript();
+        });
+
+        // add ctrl+enter shortcut to run script
+        editor.addCommand(monaco.KeyMod.CtrlCmd | monaco.KeyCode.Enter, () => {
+            executeScript();
         });
     }
 
--- src/javascript-client/webpack.config.js
+++ src/javascript-client/webpack.config.js
@@ -6,6 +6,24 @@ module.exports = {
                 enforce: 'pre',
                 use: ['source-map-loader'],
             },
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            url: true
+                        }
+                    }
+                ]
+            },
+            {
+                test: /\.ttf$/,
+                type: 'asset/resource'
+            }
         ],
     },
 };
