d9f4383eee5fd9b3a793846445c91a70ae3ad6a0
Brett V. Forsgren <brett.forsgren@outlook.com>
2026-02-15 10:09:22 -0700

    add raw JSON reading test

--- src/IxMilia.BCad.FileHandlers.Test/JsonFileHandlerTests.cs
+++ src/IxMilia.BCad.FileHandlers.Test/JsonFileHandlerTests.cs
@@ -1,3 +1,4 @@
+using System.IO;
 using System.Threading.Tasks;
 using IxMilia.BCad.Entities;
 using Xunit;
@@ -28,6 +29,58 @@ namespace IxMilia.BCad.FileHandlers.Test
         public async Task RoundTripLineTest()
         {
             await VerifyRoundTrip(new Line(new Point(1.0, 2.0, 0.0), new Point(3.0, 4.0, 0.0)));
+        }
+
+        [Fact]
+        public async Task ReadDrawingTest()
+        {
+            var rawJson = """
+                {
+                    "floorplan": {
+                        "bnds": {
+                            "x": 0.0,
+                            "y": 0.0,
+                            "w": 0.0,
+                            "h": 0.0
+                        },
+                        "lyrs": [
+                            {
+                                "n": "0",
+                                "r": 0,
+                                "g": 0,
+                                "b": 0
+                            }
+                        ],
+                        "blks": [
+                            {
+                                "n": "*Model_Space",
+                                "h": null,
+                                "ents": [
+                                    {
+                                        "t": "L",
+                                        "h": null,
+                                        "p": null,
+                                        "l": "0",
+                                        "d": "1,2,3|4,5,6"
+                                    }
+                                ]
+                            }
+                        ]
+                    }
+                }
+                """;
+            using var ms = new MemoryStream();
+            using var writer = new StreamWriter(ms);
+            await writer.WriteAsync(rawJson);
+            await writer.FlushAsync();
+            ms.Seek(0, SeekOrigin.Begin);
+            var drawingResult = await FileHandler.ReadDrawing("filename", ms, Workspace.FileSystemService.ReadAllBytesAsync);
+            Assert.True(drawingResult.Success);
+            Assert.Equal(1, drawingResult.Drawing.Layers.Count);
+            var entity = Assert.Single(drawingResult.Drawing.Layers.GetValue("0").GetEntities());
+            var line = Assert.IsType<Line>(entity);
+            Assert.Equal(new Point(1.0, 2.0, 3.0), line.P1);
+            Assert.Equal(new Point(4.0, 5.0, 6.0), line.P2);
         }
     }
 }
